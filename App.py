76d074fedeacf091b1fa71e18524618d17836099
João Mendes <joao.mendes@parvi.com.br>
2026-05-05 17:26:07 -0300

    Resolvendo Bug

--- App.py
+++ App.py
@@ -278,3 +278,6 @@ if __name__ == '__main__':
     os.makedirs('static/uploads', exist_ok=True)
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, debug=False, host='0.0.0.0', port=port)
+
+
+    ## teste de envio
